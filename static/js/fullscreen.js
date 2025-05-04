e9e714534fcbccbf3f1f8b9f3fe8e0df167229bd
apikbanyubasa <apikby657@gmail.com>
2025-05-04 16:58:49 +0700

    final

--- static/js/fullscreen.js
+++ static/js/fullscreen.js
@@ -1,11 +1,13 @@
 function toggleFullscreen(id) {
-    const elem = document.getElementById(id);
-    if (!document.fullscreenElement) {
-      elem.requestFullscreen().catch((err) => {
-        alert(`Gagal masuk fullscreen: ${err.message}`);
-      });
-    } else {
-      document.exitFullscreen();
-    }
+  const elem = document.getElementById(id);
+  if (!document.fullscreenElement) {
+    elem.requestFullscreen().then(() => {
+      elem.classList.add("fullscreen-mode");
+    }).catch((err) => {
+      alert(`Gagal masuk fullscreen: ${err.message}`);
+    });
+  } else {
+    document.exitFullscreen();
+    elem.classList.remove("fullscreen-mode");
   }
-  
+}
